4ce6f2ab081f94b5cccb6882a1ff8d3cbd1cc145
sagebeme <eudicas5k@gmail.com>
2025-06-04 11:30:21 +0300

    Javascript exercise switching variables

---
+++ javaScript/variables.js
@@ -0,0 +1,39 @@
+// Instructions
+// Write a program that switches the values stored in the variables a and b inside the main.swift file.
+
+// Warning. Do not change the code on lines 1-4 and 16-20. Your program should work for different inputs. e.g. any value of a and b.
+
+// Example Input 1
+// a: 5
+// b: 8
+// Example Output 1
+// a: 8
+// b: 5
+// Your code should also work for any value. For example if a has the value "Hello" and b is "Jack" then:
+
+// Example Input 2
+// a: "Hello"
+// b: "Jack"
+// Example Output 2
+// a: "Jack"
+// b: "Hello"
+// Hint
+// You should not have to type any numbers in your code.
+// You might need to make some more variables.
+
+function test(){
+	const noOne = "3";
+	const noTwo = "22"
+
+	/*don't change the code above*/
+	//write your code in line 7 - 9;
+
+
+
+	/*don't change the code below*/
+
+	console.log('noOne is ' + noOne);
+	console.log('noTwo is ' + noTwo);
+}
+
+test()

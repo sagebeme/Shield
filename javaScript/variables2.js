d8b89ab04acf14fbe8f8d3f32414aa69a2945a6e
sagebeme <eudicas5k@gmail.com>
2025-06-04 11:40:13 +0300

    Javascript variable info

---
+++ javaScript/variables2.js
@@ -0,0 +1,32 @@
+// Objective:
+// Store and display basic personal information using variables.
+
+// Instructions:
+// Create 3 variables:
+
+// One for your name (use a string).
+
+// One for your age (use a number).
+
+// One for whether you are a student or not (use a boolean — true or false).
+
+// Use console.log() to display this information in the following format:
+
+/* ---------------- Task 1: Personal Info Card ---------------- */
+
+function personalInfoCard() {
+  // name
+  // age
+  // isStudent
+  // 👉 YOUR CODE HERE
+
+
+
+
+  // 👆 YOUR CODE ENDS HERE
+  console.log("Name: " + name);
+  console.log("Age: " + age);
+  console.log("Student: " + isStudent);
+}
+
+personalInfoCard();

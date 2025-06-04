e9b1befba13a4de679b78f7c144f62ef7f0f58ad
sagebeme <eudicas5k@gmail.com>
2025-06-04 11:47:34 +0300

    simple maths with variables

---
+++ javaScript/variables3.js
@@ -0,0 +1,33 @@
+// Do Some Simple Math
+// Objective:
+// Practice using number variables and math operations.
+
+// Instructions:
+// Create two number variables (e.g., num1 and num2).
+
+// Sum
+
+// Difference
+
+// Product
+
+// Quotient
+
+// Example Format:
+
+// Tip:
+// Use the +, -, *, and / operators.
+
+/* ---------------- Task 2: Simple Math ---------------- */
+
+function simpleMath() {
+  // 👉 YOUR CODE HERE
+ 
+
+ 
+ // 👆 YOUR CODE ENDS HERE
+  console.log("Sum: " + (num1 + num2));
+  console.log("Difference: " + (num1 - num2));
+  console.log("Product: " + (num1 * num2));
+  console.log("Quotient: " + (num1 / num2));
+}

6dab4f22094651633bd03d7e002307bd94ca9063
sagebeme <eudicas5k@gmail.com>
2025-06-04 11:47:34 +0300

    favourite things making a sentence

---
+++ javaScript/variables4.js
@@ -0,0 +1,23 @@
+// My Favorite Things
+// Objective:
+// Store your favorite things in variables and build sentences from them.
+
+// Instructions:
+// Create 3 string variables:
+
+// Your favorite color
+
+// Your favorite movie
+
+// Your favorite food
+
+function favoriteThings() {
+  // 👉 YOUR CODE HERE
+ 
+
+
+ 
+  // 👆 YOUR CODE ENDS HERE
+  console.log("My favorite color is " + favoriteColor + ".");
+  console.log("I love watching " + favoriteMovie + " and eating " + favoriteFood + "!");
+}
